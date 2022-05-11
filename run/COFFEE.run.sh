e20798d0de26a8d5fe433852bd595a8a00ea3522
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-05-11 16:31:28 +0200

    Added verbose to coffee.run

--- run/COFFEE.run.sh
+++ run/COFFEE.run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+echo "> INSTALLING nvim <"
 ~/.local/bin/coffee run nvim
+echo "> INSTALLING vim-plug <"
 ~/.local/bin/coffee run vim-plug
+echo "> INSTALLING oh-my-zsh <"
 ~/.local/bin/coffee run omz
+echo "> INSTALLING cbonsai <"
 ~/.local/bin/coffee run cbonsai
