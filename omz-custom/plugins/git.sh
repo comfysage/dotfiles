ce25e7dd100b36b9f04a8602c07610419e6a3512
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-07-11 12:44:20 +0200

    zsh: git functions plugin

---
+++ omz-custom/plugins/git.sh
@@ -0,0 +1,40 @@
+is_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
+_gb() {
+  is_git_repo || return
+  echo "$( git branch --show-current )"
+}
+
+_git_ahead() {
+  is_git_repo || return
+  num=$(git log --oneline @{u}..HEAD | grep -cE ".*")
+  if [[ num -gt 0 ]]
+  then
+    echo " ~$num "
+  else
+    echo ""
+  fi
+}
+
+_git_dirty() {
+  local STATUS
+  local -a FLAGS
+  FLAGS=('--porcelain')
+  if [[ "${DISABLE_UNTRACKED_FILES_DIRTY:-}" == "true" ]]
+  then
+    FLAGS+='--untracked-files=no'
+  fi
+  STATUS=$(git status ${FLAGS} 2> /dev/null | tail -n 1)
+  if [[ -n $STATUS ]]
+  then
+    echo "*"
+  else
+    echo ""
+  fi
+}
+
+_git_prompt_info() {
+  echo "$(_gb)$(_git_ahead)"
+}
