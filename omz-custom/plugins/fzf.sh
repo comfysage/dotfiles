e6aaa74e838bd4e47030ae9ddd24c86d07e761b5
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-12-12 17:19:33 +0100

    feat(omz/fzf): config file for fzf opts

---
+++ omz-custom/plugins/fzf.sh
@@ -0,0 +1,7 @@
+export FZF_DEFAULT_OPTS='--height=40% --margin=0,2 --prompt="$ " --info=hidden'
+
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
+ --color=fg:white,bg:0,hl:0
+ --color=fg+:cyan,bg+:0,hl+:0
+ --color=info:cyan,prompt:magenta,pointer:green
+ --color=marker:yellow,spinner:red,header:red"
