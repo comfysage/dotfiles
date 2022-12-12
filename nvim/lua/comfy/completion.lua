22581388c8ffce45d8bc04533b3dd67c3123d987
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-12-12 19:00:59 +0100

    feat(nvim/cmp): added dictionary setup

--- nvim/lua/comfy/completion.lua
+++ nvim/lua/comfy/completion.lua
@@ -94,3 +94,32 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protoc
 require('lspconfig')['tsserver'].setup {
   capabilities = capabilities
 }
+
+require("cmp_dictionary").setup({
+	dic = {
+		-- ["*"] = { "/usr/share/dict/words" },
+		["markdown"] = { "/usr/share/dict/words" },
+		--[[ ["lua"] = "path/to/lua.dic",
+		["javascript,typescript"] = { "path/to/js.dic", "path/to/js2.dic" }, ]]
+		--[[ filename = {
+			["xmake.lua"] = { "path/to/xmake.dic", "path/to/lua.dic" },
+		}, ]]
+		--[[ filepath = {
+			["%.tmux.*%.conf"] = "path/to/tmux.dic"
+		}, ]]
+		spelllang = {
+			en = "/usr/share/dict/american-english",
+			fr = "/usr/share/dict/fr.dict",
+			nl = "/usr/share/dict/nl.dict",
+		},
+	},
+	-- The following are default values.
+	exact = 2,
+	first_case_insensitive = false,
+	document = false,
+	document_command = "wn %s -over",
+	async = false, 
+	max_items = -1,
+	capacity = 5,
+	debug = false,
+})
