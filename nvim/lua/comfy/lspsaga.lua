40b1aee4f1808ebcb8450f56afae15fac1f4de26
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-12-12 17:46:38 +0100

    feat(nvim/lua/lspsaga): added lua config for lspsaga

---
+++ nvim/lua/comfy/lspsaga.lua
@@ -0,0 +1,26 @@
+
+local saga = R'lspsaga'
+
+saga.init_lsp_saga {
+  -- error_sign = '*',
+  -- warn_sign = '*',
+  -- hint_sign = '#',
+  -- infor_sign ='*',
+  diagnostic_header = { "* ", "! ", "# ", "* " },
+  border_style = 'rounded',
+
+  code_action_icon = " ",
+  code_action_lightbulb = {
+      cache_code_action = true,
+      enable = false,
+      enable_in_insert = false,
+      sign = false,
+      sign_priority = 40,
+      update_time = 150,
+      virtual_text = false
+  }
+}
+
+vim.cmd([[
+hi! link LspSagaFinderSelection Search
+]])
