82e7939925a207b734b83a650bd540a53a713d18
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-12-13 18:05:06 +0100

    add(nvim/snip/lua): added pcall snippet

--- nvim/snip/lua.lua
+++ nvim/snip/lua.lua
@@ -1,3 +1,6 @@
 return {
-  s("req", fmt([[local {} = R '{}']], { i(1), i(2) }))
+  s("req", fmt([[local {} = R '{}']], { i(1), i(2) })),
+  s("pcall", fmt([[local status, _ = pcall(require, '{}')
+if not status then return end
+  ]], { i(1) }))
 }
