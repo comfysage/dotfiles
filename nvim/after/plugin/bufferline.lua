186eabcc22cf1daf83d24c2077cb5802e73e81b3
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-12-13 18:06:46 +0100

    feat(nvim/bufferline): added diagnostic menu to bufferline

--- nvim/after/plugin/bufferline.lua
+++ nvim/after/plugin/bufferline.lua
@@ -20,13 +20,45 @@ require "bufferline" .setup {
         show_buffer_default_icon = false, -- whether or not an unrecognised filetype should show a default icon
         show_close_icon = true,
         show_tab_indicators = true,
-        separator_style = "padded_slant"--[[ | { 'any', 'any' } ]],
+        separator_style = "padded_slant" --[[ | { 'any', 'any' } ]] ,
         hover = {
             enabled = true,
             delay = 200,
-            reveal = {'close'}
-        }
-    }
+            reveal = { 'close' }
+        },
+        groups = {
+            items = {
+                require('bufferline.groups').builtin.pinned:with({ icon = "@" })
+            }
+        },
+        custom_areas = {
+            right = function()
+                local result = {}
+                local seve = vim.diagnostic.severity
+                local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
+                local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
+                local info = #vim.diagnostic.get(0, { severity = seve.INFO })
+                local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
+
+                if error ~= 0 then
+                    table.insert(result, { text = "  " .. error .. " ", fg = "#EC5241" })
+                end
+
+                if warning ~= 0 then
+                    table.insert(result, { text = "  " .. warning .. " ", fg = "#EFB839" })
+                end
+
+                if hint ~= 0 then
+                    table.insert(result, { text = "  " .. hint .. " ", fg = "#A3BA5E" })
+                end
+
+                if info ~= 0 then
+                    table.insert(result, { text = "  " .. info .. " ", fg = "#7EA9A7" })
+                end
+                return result
+            end,
+        },
+    },
 }
 
 if vim.g.colors_name == "gruvboxed" then
