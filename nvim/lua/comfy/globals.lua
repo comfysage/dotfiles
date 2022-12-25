244356e4acfdd2b554a3f71c67e55a96cfe575f9
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-12-25 19:41:15 +0100

    add(nvim/globals): added mergetable function: MT

--- nvim/lua/comfy/globals.lua
+++ nvim/lua/comfy/globals.lua
@@ -32,3 +32,19 @@ V = function ()
   vim.notify(path)
  end)
 end
+
+MT = function (t1, t2)
+    for k,v in pairs(t2) do
+        if type(v) == "table" then
+            if type(t1[k] or false) == "table" then
+                MergeTable(t1[k] or {}, t2[k] or {})
+            else
+                t1[k] = v
+            end
+        else
+            t1[k] = v
+        end
+    end
+    return t1
+end
+
