5dbac9c4a9da0dbca4f11e85b8001e5a94200646
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-06-27 11:56:07 +0200

    feat(git): add push and changelog aliases

--- git/config.sh
+++ git/config.sh
@@ -3,6 +3,9 @@ alias _status='git status --short'
 alias status='git status -s -b'
 alias _log='PAGER=less git log --oneline --graph'
 alias ahead='git log --oneline @{u}..HEAD | grep -cE ".*"'
+alias push='git push -u origin $( git branch --show-current )'
+
+alias changelog='git -c pager.show=false show --format=" - %C(yellow)%h%C(reset) %<(80,trunc)%s" -q @@{1}..@@{0}'
 
 alias _commit='git commit' # normal commit
 alias _amend='git commit --amend' # normal commit but amend to last commit.
