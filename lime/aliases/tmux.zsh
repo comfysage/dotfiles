a604d21c78fed1183e2cef15761df1a2d8dc8c0d
comfysage <67917529+comfysage@users.noreply.github.com>
2024-03-23 14:53:59 +0100

    feat(lime/aliases/tmux): add new tmux aliases

--- lime/aliases/tmux.zsh
+++ lime/aliases/tmux.zsh
@@ -4,3 +4,6 @@ alias l="tmux list-sessions"
 alias a='(){ if [[ -n $1 ]]; then; tmux attach -t $1; else; tmux attach; fi }'
 alias ta="tmux attach -t"
 alias ts="tmux split-window -c \"#{pane_current_path}\" -p 20 \"make test; read\""
+
+alias la='(){ name=$(tmux ls -F "#S" | fzf); [[ -z "$name" ]] || a $name; }'
+alias lsa='(){ dir="$(realpath ${1:-${XDG_DEV_DIR}})"; spath=$(find "$dir" -mindepth 1 -maxdepth 1 -type d | fzf); [[ -z "$spath" ]] || sa "$spath" }'
