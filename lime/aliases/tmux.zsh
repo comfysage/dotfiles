8ba8bc4bf25172a58526c20cdf338f4bdda3e4df
comfysage <67917529+comfysage@users.noreply.github.com>
2024-03-23 14:53:47 +0100

    fix(lime/aliases/tmux): fix tmux aliases

--- lime/aliases/tmux.zsh
+++ lime/aliases/tmux.zsh
@@ -1,5 +1,5 @@
-alias s='(){ name=${1:-$(basename $PWD)}; tmux new-session -s ${name} }'
-alias sa='(){ spath=${1:-$PWD}; tmux new-session -s "$(basename $spath)" -c "$spath" }'
+alias s='(){ name="${1:-$(basename $PWD)}"; tmux new-session -s ${name} }'
+alias sa='(){ spath=${1:-$PWD}; tmux new-session -s "$(basename $spath)" -c "$(realpath $spath)" }'
 alias l="tmux list-sessions"
 alias a='(){ if [[ -n $1 ]]; then; tmux attach -t $1; else; tmux attach; fi }'
 alias ta="tmux attach -t"
