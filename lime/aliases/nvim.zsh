a4f0a2f0bf733f2d8b42450a8f1d6b1879a4a0b2
comfysage <67917529+comfysage@users.noreply.github.com>
2024-03-23 14:53:11 +0100

    feat(lime/aliases/nvim): add neovide alias

--- lime/aliases/nvim.zsh
+++ lime/aliases/nvim.zsh
@@ -5,3 +5,6 @@ alias v='(){ f "$1" && nvim --server $VISERVER --remote "$1" }'
 alias vt='nvim --server $VISERVER --remote-tab '
 alias vc='(){ nvim --server $VISERVER --remote-send ":$1<CR>" }'
 
+if command -v neovide 1>/dev/null; then
+  alias nv='neovide'
+fi
