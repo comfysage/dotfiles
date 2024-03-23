8c6ac709f610664703736f6b76667caf1ccbee33
comfysage <67917529+comfysage@users.noreply.github.com>
2024-03-23 14:51:46 +0100

    fix(lime/aliases/base): fix bat

--- lime/aliases/base.zsh
+++ lime/aliases/base.zsh
@@ -3,6 +3,9 @@ alias ls="list -a"
 alias tree="list --tree --level 3"
 alias less="$PAGER"
 
-alias bat='\bat --pager=less --color=always --style=changes --theme=base16'
 alias batl='bat --paging=never -l log --plain'
 alias bat-open='(){ [[ -f "$1" ]] && tail -f "$1" | batl }'
+
+batdiff() {
+    git diff --name-only --relative --diff-filter=d | xargs bat --diff
+}
