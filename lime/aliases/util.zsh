af7f2d7cd44a309f1ea82b15c8bb5afc0e7512c6
comfysage <67917529+comfysage@users.noreply.github.com>
2024-03-23 14:54:14 +0100

    feat(lime/aliases/util): add usefull aliases

--- lime/aliases/util.zsh
+++ lime/aliases/util.zsh
@@ -3,5 +3,12 @@ alias isalive='(){ [[ -z "$1" ]] && isup $SERVERIP || isup $1 }'
 alias nettest="isup 1.1.1.1"
 alias untar="tar -xvf"
 
+alias dff="df -hT /boot / /home/*"
+alias dir-usage="du -m --max-depth 1 . | sort -n -k 1"
+
 # alias cc="cc $CFLAGS"
 # alias make="make $MAKEOPTS"
+
+alias help='(){ eval "$1 --help" | bat -l help -p }'
+alias fntls="fc-list | grep scf | grep -oE ': (\w|\s)*' | sort -u | less"
+alias dispatch='(){ nohup $@ & disown }'
